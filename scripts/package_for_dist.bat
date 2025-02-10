0a9f4256acce92d8ce93e0ca5f94220fe1413b81
BinarySemaphore <buetowbp@gmail.com>
2025-02-10 00:03:36 -0600

    Updated dist script (win) to run in scripts or root dir

--- scripts/package_for_dist.bat
+++ scripts/package_for_dist.bat
@@ -3,22 +3,46 @@ setlocal
 
 echo Package Build for Distribution
 
+:: Figure out where we're running
+echo Checking Location...
+set src_dir=.\src
+set build_dir=.\build
+set dist_dir=.\dist
+
+if not exist %src_dir% (
+	echo Running from project script folder
+	set src_dir=.%src_dir%
+	set build_dir=.%build_dir%
+	set dist_dir=.%dist_dir%
+) else (
+	echo Running from project root
+)
+if not exist %src_dir% (
+	echo Cannot find source folder "src" from here or one folder above
+	echo Make sure your running this script from the project folder
+	pause
+	exit /b 1
+)
+
+echo:
+
 :: Get dist info
 set /p platform="Platform (windows | linux): "
 if not "%platform%"=="windows" if not "%platform%"=="w" if not "%platform%"=="linux" if not "%platform%"=="l" (
 	echo Unrecognized plaform: %platform%
+	pause
 	exit /b 1
 )
 
 set /p arch="Architecture (x86 | x64 | arm32 | arm64): "
 if not "%arch%"=="x86" if not "%arch%"=="x64" if not "%arch%"=="arm32" if not "%arch%"=="arm64" (
 	echo Unrcognized architecture: %arch%
+	pause
 	exit /b 1
 )
 
 :: Find lib file and config dist output name
 echo Finding Built Library...
-set build_dir=..\build
 set lib_name=SDL_EXT_GLSL
 set lib_final_name=SDL_EXT_GLSL
 
@@ -48,6 +72,7 @@ for %%p in (linux l) do (
 set libpath=%build_dir%\%lib_name%
 if not exist %libpath% (
 	echo Could not find built library: %libpath%
+	pause
 	exit /b 1
 )
 
@@ -56,40 +81,41 @@ echo:
 
 :: Start assembling dist folder
 echo Setup dist...
-if not exist ..\dist (
+if not exist %dist_dir% (
 	echo Create dist
-	mkdir ..\dist
+	mkdir %dist_dir%
 )
-if not exist ..\dist\SDL_EXT_GLSL (
+if not exist %dist_dir%\SDL_EXT_GLSL (
 	echo Create dist\SDL_EXT_GLSL
-	mkdir ..\dist\SDL_EXT_GLSL
+	mkdir %dist_dir%\SDL_EXT_GLSL
 	echo next
 ) else (
 	echo Dist already exists
 )
-if not exist ..\dist\SDL_EXT_GLSL\include (
+if not exist %dist_dir%\SDL_EXT_GLSL\include (
 	echo Adding include headers
-	mkdir ..\dist\SDL_EXT_GLSL\include
-	copy ..\src\*.h ..\dist\SDL_EXT_GLSL\include\
+	mkdir %dist_dir%\SDL_EXT_GLSL\include
+	copy %src_dir%\*.h %dist_dir%\SDL_EXT_GLSL\include\
 ) else (
 	echo Dist already has includes
 	set /p update="Update includes? (y/n): "
 	if "%update%"=="y" (
 		echo Updated includes
-		copy ..\src\*.h ..\dist\SDL_EXT_GLSL\include\
+		copy %src_dir%\*.h %dist_dir%\SDL_EXT_GLSL\include\
 	)
 )
-if not exist ..\dist\SDL_EXT_GLSL\%lib_final_name% (
+if not exist %dist_dir%\SDL_EXT_GLSL\%lib_final_name% (
 	echo Adding static library for %platform% %arch% as %lib_final_name%
-	copy %libpath% ..\dist\SDL_EXT_GLSL\%lib_final_name%
+	copy %libpath% %dist_dir%\SDL_EXT_GLSL\%lib_final_name%
 ) else (
 	echo Dist already has %lib_final_name% for %platform% %arch%
 	set /p update="Update lib? (y/n): "
 	if "%update%"=="y" (
 		echo Updated lib %lib_final_name%
-		copy %libpath% ..\dist\SDL_EXT_GLSL\%lib_final_name%
+		copy %libpath% %dist_dir%\SDL_EXT_GLSL\%lib_final_name%
 	)
 )
 
 echo Done
 endlocal
+pause
